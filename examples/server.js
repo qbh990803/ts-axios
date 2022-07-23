415dc4a9dd3a19393c4b33a818a142a69242849d
Simon <43344531+qbh990803@users.noreply.github.com>
2022-07-23 12:47:48 +0800

    ✨ feat(examples): 修改端口号，解决端口号占用问题

--- examples/server.js
+++ examples/server.js
@@ -12,51 +12,40 @@ const compiler = webpack(webpackConfig)
 app.use(router)
 
 app.use(
-  webpackDevMiddleware(
-    compiler,
-    {
-      publicPath: '/__build__',
-      stats: {
-        colors: true,
-        chunks: false
-      }
+  webpackDevMiddleware(compiler, {
+    publicPath: '/__build__',
+    stats: {
+      colors: true,
+      chunks: false
     }
-  )
+  })
 )
 
-app.use(
-  webpackHotMiddleware(compiler)
-)
+app.use(webpackHotMiddleware(compiler))
 
-app.use(
-  express.static(__dirname)
-)
+app.use(express.static(__dirname))
 
-app.use(
-  bodyParser.json()
-)
+app.use(bodyParser.json())
 
-app.use(
-  bodyParser.urlencoded({ extended: true })
-)
+app.use(bodyParser.urlencoded({ extended: true }))
 
-router.get('/simple/get', function (req, res) {
+router.get('/simple/get', function(req, res) {
   res.json({
     msg: `hello world`
   })
 })
 
-router.get('/base/get', function (req, res) {
+router.get('/base/get', function(req, res) {
   res.json(req.query)
 })
 
-router.post('/base/post', function (req, res) {
+router.post('/base/post', function(req, res) {
   res.json(req.body)
 })
 
-router.post('/base/buffer', function (req, res) {
+router.post('/base/buffer', function(req, res) {
   let msg = []
-  req.on('data', (chunk) => {
+  req.on('data', chunk => {
     if (chunk) {
       msg.push(chunk)
     }
@@ -67,10 +56,7 @@ router.post('/base/buffer', function (req, res) {
   })
 })
 
-const port = process.env.PORT || 5000
-module.exports = app.listen(
-  port,
-  () => {
-    console.log(`Server listening on http://localhost:${port}, Ctrl + C to stop`)
-  }
-)
+const port = process.env.PORT || 8080
+module.exports = app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}, Ctrl + C to stop`)
+})
